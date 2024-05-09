54de7fdbc1ff27f82e2cd7f3e030e9a4f006ea5c
Duong1311 <thanhduong2001@icloud.com>
2024-05-09 19:09:38 +0700

    tao cau truc thu muc

--- Frontend/src/pages/lesson/CourseLesson.jsx
+++ Frontend/src/pages/lesson/CourseLesson.jsx
@@ -1,76 +1,54 @@
 import { AiOutlineRight } from "react-icons/ai";
-import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { convertFromRaw, convertToRaw, EditorState } from "draft-js";
-import draftToHtml from "draftjs-to-html";
+// import draftToHtml from "draftjs-to-html";
 // import draftToMarkdown from "draftjs-to-markdown";
 import { useEffect, useState } from "react";
-import Markdown from "react-markdown";
-import rehypeRaw from "rehype-raw";
+// import Markdown from "react-markdown";
+// import rehypeRaw from "rehype-raw";
 import { useParams } from "react-router-dom";
 
-import {
-  createLessonContent,
-  getLessonContent,
-} from "~/services/courseServices";
-import { toast } from "react-toastify";
+import { getLessonContent } from "~/services/courseServices";
+import EditorLesson from "./Editor/EditorLesson";
 
 export default function CourseLesson() {
   const { id } = useParams();
   const [lesson, setLesson] = useState({});
+  const [old, setOld] = useState({});
 
-  const [editorState, setEditorState] = useState(null);
-
-  const [dataUpdate, setDataUpdate] = useState("");
-
-  const createLessonContentApi = async (id, data) => {
-    try {
-      const res = await createLessonContent(id, data);
-      toast.success(res.data.message);
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
   const getLessonContentApi = async (id) => {
     try {
       const res = await getLessonContent(id);
       setLesson(res.data);
-      const old = {
-        entityMap: res.data.entityMap ? res.data.entityMap : {},
-        blocks: res.data.blocks,
-      };
+      console.log(res.data);
+
       // Convert data to EditorState
-      const contentState = convertFromRaw(old);
-      const initialState = EditorState.createWithContent(contentState);
-      // Set the initial state of the editor
-      setEditorState(initialState);
-      console.log("res", res.data);
+      let a = {
+        entityMap: res.data?.entityMap ? res.data.entityMap : {},
+        blocks: res.data?.blocks
+          ? res.data.blocks
+          : [
+              {
+                key: "637gr",
+                text: "Hãy nhập thông tin về bài học.",
+                type: "unstyled",
+                depth: 0,
+                inlineStyleRanges: [],
+                entityRanges: [],
+                data: {},
+              },
+            ],
+      };
+      setOld(a);
     } catch (error) {
       console.log("error", error);
     }
   };
+  console.log(old);
 
-  const handleCreateLessonContent = () => {
-    createLessonContentApi(id, dataUpdate);
-  };
-
-  const handleOnChange = (e) => {
-    setEditorState(e);
-
-    // setDataUpdate(draftToMarkdown(convertToRaw(e.getCurrentContent())));
-    setDataUpdate(convertToRaw(e.getCurrentContent()));
-  };
-  const embedVideoCallBack = (link) => {
-    if (link.indexOf("youtube") >= 0) {
-      link = link.replace("watch?v=", "embed/");
-      link = link.replace("/watch/", "/embed/");
-      link = link.replace("youtu.be/", "youtube.com/embed/");
-    }
-    return link;
-  };
   useEffect(() => {
     getLessonContentApi(id);
   }, []);
+  if (!lesson) return <div>Loading...</div>;
   return (
     <div className="min-h-[960px] flex justify-center">
       <div className="w-5/6">
@@ -83,49 +61,16 @@ export default function CourseLesson() {
         </div>
         <div className="flex flex-row justify-between">
           <div className="font-semibold text-2xl mt-4 ">{lesson?.title}</div>
-          <button
-            onClick={handleCreateLessonContent}
-            className=" mr-4 rounded-lg text-white bg-blue-700 border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  px-4 font-sans text-xs font-bold uppercase hover:shadow-lg "
-          >
-            Lưu thay đổi
-          </button>
-        </div>
-        <div className="w-full flex flex-row mt-6 min-h-[100vh]">
-          <div className="w-1/2 border-2 rounded-md mr-10 ">
-            <div className="p-3  ">
-              <Editor
-                editorState={editorState}
-                onEditorStateChange={handleOnChange}
-                toolbar={{
-                  embedded: {
-                    embedCallback: embedVideoCallBack,
-                  },
-                }}
-              />
-
-              {/* <Editor
-                // defaultContentState={contentState}
-                initialContentState={contentState}
-                onContentStateChange={setContentState}
-                wrapperClassName="wrapper-class"
-                editorClassName="editor-class"
-                toolbarClassName="toolbar-class"
-                placeholder="Write something!"
-                toolbar={{
-                  embedded: {
-                    embedCallback: embedVideoCallBack,
-                  },
-                }}
-              /> */}
-            </div>
-          </div>
-          <div className="w-1/2 border-2 rounded-md">
-            <div>demo</div>
-            <Markdown rehypePlugins={[rehypeRaw]}>
-              {draftToHtml(dataUpdate)}
-            </Markdown>
+          <div>
+            <button
+              // onClick={handleCreateLessonContent}
+              className=" mr-4 rounded-lg text-white bg-blue-700 border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  px-4 font-sans text-xs font-bold uppercase hover:shadow-lg "
+            >
+              Lưu
+            </button>
           </div>
         </div>
+        <EditorLesson old={old} />
       </div>
     </div>
   );
---
+++ Frontend/src/pages/lesson/Editor/EditorLesson.jsx
@@ -0,0 +1,104 @@
+/* eslint-disable react/prop-types */
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { convertFromRaw, convertToRaw, EditorState } from "draft-js";
+// import draftToHtml from "draftjs-to-html";
+// import draftToMarkdown from "draftjs-to-markdown";
+import { useEffect, useState } from "react";
+// import Markdown from "react-markdown";
+// import rehypeRaw from "rehype-raw";
+import { useParams } from "react-router-dom";
+
+import { createLessonContent } from "~/services/courseServices";
+import { toast } from "react-toastify";
+
+export default function EditorLesson({ old }) {
+  const { id } = useParams();
+
+  const [editorState, setEditorState] = useState(null);
+
+  const [dataUpdate, setDataUpdate] = useState("");
+
+  const createLessonContentApi = async (id, data) => {
+    try {
+      const res = await createLessonContent(id, data);
+      toast.success(res.data.message);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+  const handleCreateLessonContent = () => {
+    createLessonContentApi(id, dataUpdate);
+  };
+  const handleOnChange = (e) => {
+    setEditorState(e);
+
+    // setDataUpdate(draftToMarkdown(convertToRaw(e.getCurrentContent())));
+    setDataUpdate(convertToRaw(e.getCurrentContent()));
+  };
+  const embedVideoCallBack = (link) => {
+    if (link.indexOf("youtube") >= 0) {
+      link = link.replace("watch?v=", "embed/");
+      link = link.replace("/watch/", "/embed/");
+      link = link.replace("youtu.be/", "youtube.com/embed/");
+    }
+    return link;
+  };
+
+  useEffect(() => {
+    if (old.blocks) {
+      const contentState = convertFromRaw(old);
+      const initialState = EditorState.createWithContent(contentState);
+      // Set the initial state of the editor
+      setEditorState(initialState);
+    }
+  }, [old]);
+
+  return (
+    <div className="w-full flex flex-col mt-6 min-h-[100vh]">
+      <div className="flex flex-row justify-between mb-3">
+        <div></div>
+        <button
+          onClick={handleCreateLessonContent}
+          className=" max-w-[5rem] h-10 rounded-lg text-white bg-blue-700 border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  px-4 font-sans text-xs font-bold uppercase hover:shadow-lg "
+        >
+          Lưu
+        </button>
+      </div>
+      <div className="w-full border-2 rounded-md  ">
+        <div className="p-3  ">
+          <Editor
+            editorState={editorState}
+            onEditorStateChange={handleOnChange}
+            toolbar={{
+              embedded: {
+                embedCallback: embedVideoCallBack,
+              },
+            }}
+          />
+
+          {/* <Editor
+                // defaultContentState={contentState}
+                initialContentState={contentState}
+                onContentStateChange={setContentState}
+                wrapperClassName="wrapper-class"
+                editorClassName="editor-class"
+                toolbarClassName="toolbar-class"
+                placeholder="Write something!"
+                toolbar={{
+                  embedded: {
+                    embedCallback: embedVideoCallBack,
+                  },
+                }}
+              /> */}
+        </div>
+      </div>
+      {/* <div className="w-1/2 border-2 rounded-md">
+            <div>demo</div>
+            <Markdown rehypePlugins={[rehypeRaw]}>
+              {draftToHtml(dataUpdate)}
+            </Markdown>
+          </div> */}
+    </div>
+  );
+}
