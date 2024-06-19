c56853cb39c434d1b49a15b5ff87deba6a3834f8
Duong1311 <thanhduong2001@icloud.com>
2024-06-19 22:06:43 +0700

    fix

--- Backend/routes/course.js
+++ Backend/routes/course.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const courseValidation = require("../validations/courseValidation");
+// const courseValidation = require("../validations/courseValidation");
 const courseController = require("../controllers/courseControllers");
 const multer = require("multer");
 // const { upload } = require("../utils/multerStore");
