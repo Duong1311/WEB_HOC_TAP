2014560aa5cc52f8c04d2cc63a8f2085919d9e4f
Duong1311 <thanhduong2001@icloud.com>
2024-06-30 14:59:03 +0700

    fix url

--- Frontend/src/utils/CustomizeAxios.js
+++ Frontend/src/utils/CustomizeAxios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api/",
-  // baseURL: "https://api.gptacademy.io.vn/api/",
+  // baseURL: "http://localhost:3000/api/",
+  baseURL: "https://api.gptacademy.io.vn/api/",
 
   // timeout: 100000,
   headers: {
