311480c2abfc1d0185768dd2185fc1618a368033
Duong1311 <thanhduong2001@icloud.com>
2024-06-30 11:54:38 +0700

    fix

---
+++ Backend/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "backend",
+      script: "./index.js",
+      env: {
+        NODE_ENV: "development",
+        PORT: 3000,
+      },
+    },
+  ],
+};
