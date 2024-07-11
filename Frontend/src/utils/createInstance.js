d4a085d0e9fe14da826bf0445ae76570afc84f6d
Duong1311 <thanhduong2001@icloud.com>
2024-07-11 18:30:37 +0700

    fix url

--- Frontend/src/utils/createInstance.js
+++ Frontend/src/utils/createInstance.js
@@ -4,8 +4,8 @@ import axios from "axios";
 // axios.defaults.withCredentials = true;
 import { jwtDecode } from "jwt-decode";
 axios.defaults.withCredentials = true;
-// export const URL = "https://api.gptacademy.io.vn/api/";
-export const URL = "http://localhost:3000/api/";
+export const URL = "https://api.gptacademy.io.vn/api/";
+// export const URL = "http://localhost:3000/api/";
 
 const refreshToken = async () => {
   try {
