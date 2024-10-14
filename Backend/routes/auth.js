6c66cd40ee776714857862dc10e8113db21e2b7f
Duong1311 <thanhduongqwaszx@gmail.com>
2024-10-14 17:37:59 +0700

    fix

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -7,7 +7,9 @@ const router = express.Router();
 router.post("/register", authControllers.registerUser);
 router.post("/login", authControllers.loginUser);
 router.post("/refresh", authControllers.refreshToken);
-router.post("/logout", middleware.verifyToken, authControllers.logout);
+router.post("/logout", authControllers.logout);
+// router.post("/logout", middleware.verifyToken, authControllers.logout);
+
 router.post("/google-auth", authControllers.loginUserGoogle);
 
 module.exports = router;
