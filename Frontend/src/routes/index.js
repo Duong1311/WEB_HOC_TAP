bd2c8e047f22639f200ffc02ebe65c67b0d3c1b1
Duong1311 <thanhduongqwaszx@gmail.com>
2024-10-23 15:52:37 +0700

    fix

--- Frontend/src/routes/index.js
+++ Frontend/src/routes/index.js
@@ -1,7 +1,7 @@
-import CreateCourse from "~/pages/CreateCourse/CreateCourse.jsx";
+import CreateCourse from "~/pages/CreateCourses/CreateCourse.jsx";
 import GvLayout from "~/layout/GvLayout/GvLayout.jsx";
 // import CourseChapter from "../pages/Chapter/CourseChapter.jsx";
-import CourseChapter from "../pages/Chapter/CourseChapter.jsx";
+import CourseChapter from "../pages/Chapters/CourseChapter.jsx";
 
 import GvHome from "../pages/Gvhome/GvHome.jsx";
 import Home from "../pages/Home/home.jsx";
